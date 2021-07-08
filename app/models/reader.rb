9f7506b1c2b5cfea840595c8b7cce87d019e0ff6
angeloxenakis <angeloxenakis@gmail.com>
2021-07-08 11:41:38 -0500

    first commit

---
+++ app/models/reader.rb
@@ -0,0 +1,16 @@
+class Reader < ActiveRecord::Base
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe(magazine, price)
+        Subscription.create(magazine_id: magazine.id, price: price, reader_id: self.id)
+    end
+
+    def total_subcription_price
+        self.subscriptions.map { |sub| sub.price }.sum
+    end
+
+    def cancel_subscription(magazine)
+        self.subscriptions.find { |sub| sub.magazine == magazine}.destroy
+    end
+end
